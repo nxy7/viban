5dc5d4d137aa53f64bc12b5d53567b8459d22dcc
Dawid Danieluk <danielukd@gmail.com>
2026-01-05 22:11:32 +0100

    Include GH CLIENT ID in release build

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -2,4 +2,6 @@ import Config
 
 config :viban, VibanWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
+config :viban, :github_client_id, System.get_env("GH_CLIENT_ID")
+
 config :logger, level: :info
